23d94706f014b262b881c4aa99568bc2bc2be6fd
PingMaster99 <59905458+PingMaster99@users.noreply.github.com>
2020-04-05 14:48:25 -0600

    Kdoc and final files

--- app/src/main/java/com/example/lab2_pabloruiz18259/MainActivity.kt
+++ app/src/main/java/com/example/lab2_pabloruiz18259/MainActivity.kt
@@ -7,47 +7,82 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import java.lang.Exception
-
+/**
+ * <h1>MainActivity</h1>
+ *<p>
+ * Generates the app and calculates the body mass index
+ *</p>
+ *
+ * @author Pablo Ruiz (PingMaster99)
+ * @version 1.0
+ * @since 2020-04-05
+ **/
 class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnLongClickListener {
 
+    // Button that processes data and user input
     lateinit var processButton: Button
     lateinit var height: EditText
     lateinit var weight: EditText
 
+    /**
+     * Builds the app initialization
+     * @param savedInstanceState saved App data
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Gets the specified elements by id
         processButton = findViewById(R.id.processButton)
         height = findViewById(R.id.height)
         weight = findViewById(R.id.weight)
 
+        // Button is set with on click listener and on long click listener
         processButton.setOnClickListener(this)
         processButton.setOnLongClickListener(this)
-
     }
 
+    /**
+     * Calculates BMI on the button press
+     * @param view that contains the listener
+     */
     override fun onClick(view: View?) {
+        // try catch avoids empty inputs
         try {
 
-            var heightDouble: Double = height.text.toString().toDouble() / 100.0
-            var weightDouble: Double = weight.text.toString().toDouble() * 0.453592
-            var calculationResult: Double = weightDouble / (heightDouble * heightDouble)
+            // Values gotten from the user input
+            val heightDouble: Double = height.text.toString().toDouble() / 100.0    // To meters
+            val weightDouble: Double = weight.text.toString().toDouble() * 0.453592 // To kilograms
+            val calculationResult: Double = weightDouble / (heightDouble * heightDouble)    // BMI
 
+            // Saves the intent to load the next activity
             val results = Intent(this, Result::class.java)
-            results.putExtra("result", calculationResult)
+            results.putExtra("result", calculationResult)   // Makes the result accessible
 
-            if(heightDouble in 0.01..2.70) {
-                startActivity(results)
+            // If the introduced height is within range
+            if(heightDouble in 0.01..2.70 && weightDouble in 1.0..1800.0) {
+                startActivity(results)  // New activity is instanced
             } else {
-                Toast.makeText(this, "Ingrese un valor entre 1 y 270 en la altura", Toast.LENGTH_SHORT).show()
+                // Error message appears according to the incorrect input
+                if(heightDouble !in 0.01..2.70) {
+                    Toast.makeText(this,
+                        "Ingrese un valor entre 1 y 270 en la altura", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this,
+                        "Ingrese un valor entre 1 y 1800 en el peso", Toast.LENGTH_SHORT).show()
+                }
             }
 
-
+        // If an input statement is empty, a message appears
         } catch(e: Exception) {
             Toast.makeText(this, "Por favor no deje entradas vacias", Toast.LENGTH_SHORT).show()
         }
     }
 
+    /**
+     * Shows the BMI index on a long press
+     * @param view that contains the listener
+     */
     override fun onLongClick(v: View?): Boolean {
         Toast.makeText(this, "IMC = Peso (kg) / altura (m) ^ 2", Toast.LENGTH_SHORT).show()
         return true
--- app/src/main/java/com/example/lab2_pabloruiz18259/Result.kt
+++ app/src/main/java/com/example/lab2_pabloruiz18259/Result.kt
@@ -2,36 +2,62 @@ package com.example.lab2_pabloruiz18259
 import kotlin.math.round
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import java.awt.font.TextAttribute
-
+/**
+ * <h1>Result</h1>
+ *<p>
+ * Shows the result of the BMI calculation
+ *</p>
+ *
+ * @author Pablo Ruiz (PingMaster99)
+ * @version 1.0
+ * @since 2020-04-05
+ **/
 class Result : AppCompatActivity() {
 
+    // Needed elements to display the result
     lateinit var imc_display: TextView
     lateinit var interpretation: TextView
     lateinit var imc_image: ImageView
+
+    /**
+     * Builds the activity when instanced
+     * @param savedInstanceState saved App data
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        // Gets the result of the BMI from the MainActivity activity
         val imc: Double = intent.getDoubleExtra("result", 0.0)
+
+        // Needed elments are found by id
         imc_display = findViewById(R.id.numeric_imc_result)
         interpretation = findViewById(R.id.imc_text)
         imc_image = findViewById(R.id.imc_visualization)
 
+        // Sets the BMI visible on screen
         imc_display.text = (round(imc * 100) / 100).toString()
 
+        // Displays the BMI result interpretation according to different ranges
         if(imc < 18.5) {
-            interpretation.text = getString(R.string.low_imc)
-            imc_image.setImageResource(R.drawable.underweight)
+            // Underweight
+            interpretation.text = getString(R.string.low_imc)   // Text
+            imc_image.setImageResource(R.drawable.underweight)  // Image
+
         } else if (imc < 25) {
+            // Ideal weight
             interpretation.text = getString(R.string.ideal_imc)
             imc_image.setImageResource(R.drawable.ideal)
+
         } else if(imc < 30) {
+            // Overweight
             interpretation.text = getString(R.string.imc_overweight)
             imc_image.setImageResource(R.drawable.overweight)
+
         } else {
+            // Obese
             interpretation.text = getString(R.string.imc_obese)
             imc_image.setImageResource(R.drawable.obesity)
         }
